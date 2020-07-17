5132dd7eda818183a42599cc7a5533d05f9fe6d0
davadeola <adeoladavid15@gmail.com>
2020-07-17 16:07:15 +0300

    can get specific user

--- controller.js
+++ controller.js
@@ -31,8 +31,29 @@ exports.addStudent = async (req, res) => {
       }
     );
   } catch (error) {
-    console.log(JSON.stringify(req.body));
+    console.log("Something went wrong " + error);
+    return res
+      .status(500)
+      .json({ success: false, serverError: "Something went wrong " });
+  }
+};
 
+exports.getStudent = async (req, res) => {
+  try {
+    const id = req.params.studentId;
+
+    await connect.query(
+      "SELECT * FROM student_detail WHERE id = ?",
+      [id],
+      (err, results, fields) => {
+        if (err) {
+          return res.status(500).json({ success: false, error: err });
+        } else {
+          return res.status(200).json({ result: results, success: true });
+        }
+      }
+    );
+  } catch (error) {
     console.log("Something went wrong " + error);
     return res
       .status(500)
--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllStudents, addStudent } = require("./controller");
+const { getAllStudents, addStudent, getStudent } = require("./controller");
 
 //define actions on the / route
 router.route("/").get(getAllStudents).post(addStudent);
+
+router.route("/:studentId").get(getStudent).put(updateStudent);
 
 module.exports = router;
